92f5c84dd48da6ee691cdf5f47a6eb63f03b87a7
Mike Garcia <mgarcial@linux-crwo.site>
2016-07-12 21:03:53 -0600

    Hello World CBT Nuggets

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+UserName = 'mgarcia'
+FullName = 'Mike Garcia'
+print(UserName)
+print(FullName)
+
+Altname = UserName
+IncorrectName = 'FullName'
+NewName = UserName = FullName
+print(NewName)
+print(UserName)
